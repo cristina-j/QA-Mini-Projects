1d5723957c6d608fa915cf79bc0d972c59db3cf1
Cristina John <cristinajohn60@gmail.com>
2025-09-18 16:39:43 -0400

    login_function.py

--- Mini Project 1/login_function.py
+++ Mini Project 1/login_function.py
@@ -1,1 +1,13 @@
+# I have written a Python script for login_validation, but now that I have learned about functions, I want to create this into a function so that it can be reusable.
 
+def validate_login (username, password):
+    valid_username = "validusername1"
+    valid_password = "validpassword1"
+
+    if username == valid_username and password == valid_password:
+        return True 
+    return False
+
+# testing the function
+print(validate_login("validusername1", "validpassword1")) #True
+print(validate_login("wrongusername", "wrongpassword")) #False
